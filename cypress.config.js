2729e266d3e97020343afe9d9cf6e2db23f14c7a
Nurik <nurik@cydeo.com>
2023-09-08 19:59:20 -0400

    add: more test in  locator

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
+    video: false, 
+    retries: 2, //If testing fails, it will try 2 more time
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
