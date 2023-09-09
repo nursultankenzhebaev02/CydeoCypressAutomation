d61dcd4a697b7c07b67b878035f84cd2b0aface3
Nurik <nurik@cydeo.com>
2023-09-08 22:38:11 -0400

    add: more test in  locator

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -16,5 +16,22 @@ describe('Context: My first test', () => {
             //Assert the text
             cy.contains('Clicked on button three!').should('be.visible');
         })
+        //You got all buttons with tagName
+        cy.get('button').each((item, index, list) => {
+            //Assert length of the list, verify number of buttons
+            expect(list).to.have.length(6);
+            expect(item).to.have.attr('onclick');
+        })
+
+        //I will get all buttons like previous approach, get only the item then check for text of each item,
+        // if it is equal to Button 4, then click on it
+        cy.get('button').each((item) => {
+         if(item.text() == 'Button 4'){
+         cy.log(item.text()); //This command write the text at the test console
+            // item.click(); you can not use cypress click func on JQuery element 
+            cy.wrap(item).click();
+            cy.contains('Clicked on button four!').should('be.visible');
+         }
+        })
     })
 })
